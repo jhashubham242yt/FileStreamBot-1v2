13d167123c3df668835867d703d6f15f8cd5fdde
Shivdwivediji <115983642+Shivdwivediji@users.noreply.github.com>
2023-08-08 06:14:10 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -8,14 +8,12 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = str(environ.get("API_HASH"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN"))
+    API_ID = int(environ.get("API_ID","25833520"))
+    API_HASH = str(environ.get("API_HASH","7d012a6cbfabc2d0436d7a09d8362af7"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN","5585418491:AAEucgRoqjH1zLWXO5Rp7-CChI6N4UMuhPM"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    BIN_CHANNEL = int(
-        environ.get("BIN_CHANNEL", None)
-    )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
+    BIN_CHANNEL = int(environ.get("BIN_CHANNEL", -1001611644647))  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -25,7 +23,7 @@ class Var(object):
     NO_PORT = True if str(NO_PORT).lower() == "true" else False
     if "DYNO" in environ:
         ON_HEROKU = True
-        APP_NAME = str(environ.get("APP_NAME"))
+        APP_NAME = str(environ.get("APP_NAME","The_Silent_Teams"))
     else:
         ON_HEROKU = False
     FQDN = (
@@ -40,11 +38,11 @@ class Var(object):
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
-    DATABASE_URL = str(environ.get('DATABASE_URL'))
+    DATABASE_URL = str(environ.get('DATABASE_URL','mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', "aredirect"))
     PAGE_LINK = environ.get('PAGE_LINK', None)
     OWNER_ID = int(environ.get('OWNER_ID', '777000'))
-    SESSION_NAME = str(environ.get('SESSION_NAME', 'F2LxBot'))
+    SESSION_NAME = str(environ.get('SESSION_NAME', 'F2L'))
     FORCE_UPDATES_CHANNEL = environ.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_UPDATES_CHANNEL = True if str(FORCE_UPDATES_CHANNEL).lower() == "true" and UPDATES_CHANNEL != 'aredirect' else False
 
